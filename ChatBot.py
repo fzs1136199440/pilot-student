d0914d0953ee0cd3d757785afb3df5a3e4d4cd94
JerryFu <fzs1136199440@icloud.com>
2020-04-12 18:55:19 +0800

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -26,7 +26,7 @@ class HelloBot(Bot):
     def _think(self, s):
         return f"Hello {s}"
 
-class GreeetingBot(Bot):
+class GreetingBot(Bot):
     def __init__(self):
         self.q = "How are you today?"
 
@@ -43,4 +43,36 @@ class favoriteColorBot(Bot):
 
     def _think(self, s):
         colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple']
-        return f"You like {s.lower()}? My favorite color is {random.choice(colors)}"
+        return f"You like {s.lower()}? My favorite color is {random.choice(colors)}."
+
+# 下面我们需要一个主流程把上面的所有 Bot 串起来（这样就可以一起运行了），这是我们的对话系统的主类定义：
+
+class Magpie:
+
+    def __init__(self, wait=1):
+         Bot.wait = wait
+         self.bots = []
+
+    def add(self, bot):
+        self.bots.append(bot) # list.append()用于在列表末尾添加新的对象。
+
+    def _prompt(self, s):
+        print(s)
+        print()
+
+    def run(self):
+        self._prompt("This is Magpie dialog system. Let's talk.")
+        for bot in self.bots:
+            bot.run()
+
+
+# 初始化一个Magpie实例来运行：
+
+# 创建一个聊天延时为 1s 的对话系统
+magpie = Magpie(1)
+# 向其中加入我们已经定义好的各个对话 bot 的对象实例
+magpie.add(HelloBot())
+magpie.add(GreetingBot())
+magpie.add(favoriteColorBot())
+# 运行它
+magpie.run()
